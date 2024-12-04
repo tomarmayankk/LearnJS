f3d3125bf4e9a910eee95c8e1b93385407f4323d
tomarmayankk <mayanktomarpc@gmail.com>
2024-12-04 16:11:57 +0530

    Loops

--- Basics/Loops.js
+++ Basics/Loops.js
@@ -1,4 +1,19 @@
 //general loops knowledge in Javascript
-function isPrime(num){
-    
+function isPrime(num) { 
+    if (num <= 1) { 
+        console.log("Not a prime number");
+        return;
+    }
+
+    for (let i = 2; i <= Math.sqrt(num); i++) { 
+        if (num % i === 0) { 
+            console.log( "Your input: " + num + "Not a prime number");
+            return;
+        }
+    }
+    console.log( "Your input: " + num + " Yes, it's a prime number");
 }
+
+// Example Usage
+isPrime(12); // Output: Not a prime number
+isPrime(7);  // Output: Yes, it's a prime number
